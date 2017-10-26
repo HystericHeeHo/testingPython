a1cdfadb61d521ce8d49c631280bc934baf924c0
Justin Adams <justintadams22@gmail.com>
2017-10-25 21:23:52 -0400

    created an if statement that takes user input. not sure if it works. have yet to test it.

---
+++ country.py
@@ -0,0 +1,8 @@
+country = input("What Country are you from? Please choose USA or Canada")
+
+if country == "USA" or "usa":
+  print("*Cue Guiles Theme* Welcome to America!")
+elif country == "Canada" or "canada":
+  print("Hello, eh.")
+else:
+  print("You have entered an incorrect choice. Please try again.")
