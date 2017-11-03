6588d4565e1ab36b7c5f8f6cb8e837c1dfc3b101
HystericHeeHo <justintadams22@gmaiL.com>
2017-11-02 23:36:15 -0400

    added numbers.py

---
+++ numbers.py
@@ -0,0 +1,18 @@
+#prints opening statement
+print("Python is Cool!" + " " "Watch this!")
+#User chooses a number.
+userNumber = input("Pick an age between 1 and 25. ")
+if int(userNumber) > 25:
+    print("Well, You're no longer in your prime.")
+elif int(userNumber) == 25:
+    print("wow you're 25? You don't look a day over 18")
+elif int(userNumber) == 23:
+    print("No one's gonna like you when you're 23")
+elif int(userNumber) < 25:
+    print("You're like almost a real adult")
+elif int(userNumber) >= 10:
+    Print("You've still got a ways to go kid.")
+elif int(userNumber) < 10:
+    print("Are you even old enough to know how to take this?")
+else:
+    print("You have entered an invalid response, Please Try Again.")
