77e7d15f0a7c72b71fbb9c73fc5c67afec75bb41
Justin Adams <JustintAdams22@gmail.com>
2018-08-07 13:28:02 -0400

    made changes to exercise and updated code

--- tstp/ch9chal3.py
+++ tstp/ch9chal3.py
@@ -1,9 +1,8 @@
 import os 
 import csv
 
-favmovies1 = ['Top Gun', 'Risky Business', 'Minority Report']
-favmovies2 = [' Titanic', 'Revenant', 'Inception']
-favmovies3 = ['Training Day', 'Man on Fire', 'Flight']
-with open("hello.csv", "w", newline='') as f:
+favmovies = [['Top Gun', 'Risky Business', 'Minority Report'], ['Titanic', 'Revenant', 'Inception'], ['Training Day', 'Man on Fire', 'Flight']]
+with open("hello.csv", "w") as f:
     w = csv.writer(f, delimiter=',')
-    w.writerow([favmovies1, favmovies2, favmovies3])
+    for movie_list in favmovies:
+        w.writerow(movie_list)
