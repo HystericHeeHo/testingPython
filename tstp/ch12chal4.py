2606da6d57a5a6c297ba6eb664a303a406f033a6
Justin Adams <JustintAdams22@gmail.com>
2018-08-16 15:18:19 -0400

    finished the challenges for chapter 12

--- tstp/ch12chal4.py
+++ tstp/ch12chal4.py
@@ -1,8 +1,13 @@
 class hexagon():
-    def __init__(self, s):
-        self.side = float(s)
+    def __init__(self,a,b,c,d,e,f):
+        self.side1 = a
+        self.side2 = b
+        self.side3 = c
+        self.side4 = d
+        self.side5 = e
+        self.side6 = f
     def calculate_perimeter(self):
-        area = ((3 ** 0)/2) * (self.side**2.0)   
-        return area
-hex1 = hexagon(5)
+        perimeter = self.side1+self.side2+self.side3+self.side4+self.side5+self.side6
+        return perimeter
+hex1 = hexagon(5,1,2,4,3,6)
 print(hex1.calculate_perimeter())        
